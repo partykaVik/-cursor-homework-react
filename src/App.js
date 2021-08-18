cca8468f5737797511a1e8ebb5e2f0777c9e2443
Viktor <partykav2017@gmail.com>
2021-08-18 21:21:03 +0300

    homework-20 added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './styles.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Posts from './components/homework-17/Posts';
+import Gallery from './components/Gallery/Gallery';
+import Contacts from './components/homework-18/Contacts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => {
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<div className="container">
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/Posts" component={Posts} />
+						<Route exact path="/Gallery" component={Gallery} />
+						<Route exact path="/Contacts" component={Contacts} />
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
+};
 export default App;
